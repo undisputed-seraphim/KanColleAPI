e36bb0c832927d291349fb11da09e3beab53fa4b
Undisputed Seraphim <undisputed.seraphim@gmail.com>
2014-06-20 02:11:27 +0800

    Added kanColleConsole for console debugging and infodumping (WIP)

---
+++ KanColleAPI/Request/Hensei.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColle.Request {
+	public class Hensei {
+
+		public static string RemoveAll (int fleet_id) {
+			return Change(-2, -1, fleet_id);
+		}
+
+		public static string Change (int ship_id, int fleet_position, int fleet_id) {
+			StringBuilder str = new StringBuilder();
+			str.AppendFormat("api_ship_id={0}&", ship_id);
+			str.AppendFormat("api_ship_idx={0}&", --fleet_position);
+			str.Append("api_token={0}&");
+			str.AppendFormat("api_verno={0}&", 1);
+			str.AppendFormat("api_id={0}", fleet_id);
+			return str.ToString();
+		}
+	}
+
+	public class Change {
+		public int? api_change_count { get; set; }
+	}
+}
---
+++ KanColleConsole/KanColleConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using KanColle;
+
+namespace KanColleConsole {
+	class KanColleConsole {
+
+		static void Main (string[] args) {
+			Console.OutputEncoding = Encoding.Unicode;
+
+			StreamReader reader = new StreamReader("api_token.txt");
+			string full_api_token = reader.ReadLine();
+			reader.Close();
+
+			KanColleProxy kcp = new KanColleProxy(full_api_token);
+		}
+	}
+}
