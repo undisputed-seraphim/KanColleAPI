ff00ad1e556af3a05ce65419c11747f9dfcf4a7c
Undisputed Seraphim <undisputed.seraphim@gmail.com>
2014-06-20 02:19:53 +0800

    Added Member.MapCell for map probing and scraping.

---
+++ KanColleAPI/Member/MapCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColle.Member {
+	public class MapCell {
+		public int api_id { get; set; }
+		public int api_passed { get; set; }
+
+		public static string MAPCELL = "api_get_member/mapcell/";
+
+		public static string MapCell (int map_area_id, int map_info_no) {
+			StringBuilder str = new StringBuilder();
+			str.AppendFormat("api_maparea_id={0}&", map_area_id);
+			str.Append("api_token={0}&");
+			str.AppendFormat("api_verno={0}&", 1);
+			str.AppendFormat("api_mapinfo_mo={0}", map_info_no);
+			return str.ToString();
+		}
+	}
+}
