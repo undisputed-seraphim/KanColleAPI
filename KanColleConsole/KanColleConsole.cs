1833d02e8ddc5994c78d1560174957247e9a5db8
Undisputed Seraphim <undisputed.seraphim@gmail.com>
2014-08-30 19:57:00 +0800

    API Port update for 30 Aug 2014. Update for RunExp possibly coming in a week.

--- KanColleConsole/KanColleConsole.cs
+++ KanColleConsole/KanColleConsole.cs
@@ -7,12 +7,14 @@ using KanColle;
 using KanColle.Request;
 using Newtonsoft.Json;
 
+using KanColleBot;
+
 namespace KanColleConsole {
 	class KanColleConsole {
 
 		static void Main (string[] args) {
 			Console.OutputEncoding = Encoding.Unicode;
-
+			
 			StreamReader reader = new StreamReader("api_token.txt");
 			string full_api_token = reader.ReadLine();
 			reader.Close();
