4f3e5d843bebeaf54d5c61fe0dc8821036c992df
Tan Li Boon <undisputed.seraphim@gmail.com>
2015-06-02 16:03:01 +0800

    Commit before I go home

--- RunExpKai/MainWindow.xaml.cs
+++ RunExpKai/MainWindow.xaml.cs
@@ -13,6 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Newtonsoft.Json;
+
+using KanColle;
+using KanColle.Request.Mission;
+
+
 namespace RunExpKai {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
@@ -21,6 +27,12 @@ namespace RunExpKai {
 		public MainWindow() {
 			InitializeComponent();
 		}
+
+		// Members
+		KanColleProxy kcproxy;
+		bool fleet_2_isrunning, fleet_3_isrunning, fleet_4_isrunning;
+
+		// Worker threads
 
 		private void Fleet_2_Select_SelectionChanged( object sender, SelectionChangedEventArgs e ) {
 
@@ -56,6 +68,31 @@ namespace RunExpKai {
 
 		private void Fleet_4_Stop_Click( object sender, RoutedEventArgs e ) {
 
+		}
+
+
+		private void run() {
+
+		}
+
+		private void result(int fleet_id) {
+			string parameter = Mission.Result( fleet_id );
+			string post_response = this.kcproxy.proxy( Mission.RESULT, parameter );
+
+			KanColleAPI<MissionResult> result = JsonConvert.DeserializeObject<KanColleAPI<MissionResult>>( post_response );
+
+			// If mission fails, abort completely.
+			if (result.GetData().GetResult().Equals( ExpeditionResult.FAIL )) {
+				if (fleet_id == 2) { this.fleet_2_isrunning = false; }
+				if (fleet_id == 3) { this.fleet_3_isrunning = false; }
+				if (fleet_id == 4) { this.fleet_4_isrunning = false; }
+			} else {
+				int[] GetMaterials = result.GetData().PrintConsole();
+
+			}
+		}
+
+		private void refuel() {
 		}
 	}
 }
