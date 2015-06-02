ef8748407ca847c59904ec8704d70a9f5dc5b418
Tan Li Boon <undisputed.seraphim@gmail.com>
2015-06-02 14:28:35 +0800

    Created NEW RunExpKai! Now with WPF!

---
+++ RunExpKai/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RunExpKai {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void Fleet_2_Select_SelectionChanged( object sender, SelectionChangedEventArgs e ) {
+
+		}
+
+		private void Fleet_3_Select_SelectionChanged( object sender, SelectionChangedEventArgs e ) {
+
+		}
+
+		private void Fleet_4_Select_SelectionChanged( object sender, SelectionChangedEventArgs e ) {
+
+		}
+
+		private void Fleet_2_Start_Click( object sender, RoutedEventArgs e ) {
+
+		}
+
+		private void Fleet_3_Start_Click( object sender, RoutedEventArgs e ) {
+
+		}
+
+		private void Fleet_4_Start_Click( object sender, RoutedEventArgs e ) {
+
+		}
+
+		private void Fleet_2_Stop_Click( object sender, RoutedEventArgs e ) {
+
+		}
+
+		private void Fleet_3_Stop_Click( object sender, RoutedEventArgs e ) {
+
+		}
+
+		private void Fleet_4_Stop_Click( object sender, RoutedEventArgs e ) {
+
+		}
+	}
+}
